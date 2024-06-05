e87b8aa8191caeea6e3731f3dad4e6dc4b7c8406
MickyTassew <mikiyastassew20@gmail.com>
2024-06-05 16:42:59 -0500

    final amazon commit

--- amazon-frontend/src/App.jsx
+++ amazon-frontend/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Routing from './Router'
+import { DataContext } from './components/DataProvider/DataProvider'
+import { Type } from './Utility/action.type';
+import {auth} from './Utility/firebase'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [{user}, dispatch] = useContext(DataContext)
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        console.log(authUser);
+        dispatch({
+          type:Type.SET_USER,
+          user:authUser
+        })
+      }else{
+        dispatch({
+          type:Type.SET_USER,
+          user: null,
+        })
+      }
+    })
+
+
+
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routing />
     </>
   )
 }
